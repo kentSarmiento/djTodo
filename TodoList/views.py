0a8e15305a22b03ff3a3e768dd0fd08e5a790410
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-03-22 00:09:39 +0900

    Support view and create tasks

--- TodoList/views.py
+++ TodoList/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    context={}
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            context={'all_items': all_items}
+
+        return render(request, 'home.html', context)
+
+    else:
+        all_items = List.objects.all
+        context={'all_items': all_items}
+        return render(request, 'home.html', context)
